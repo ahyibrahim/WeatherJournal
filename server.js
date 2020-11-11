f64f659c0790206d601c97a92d3184dd1101c18d
ahyibrahim <57627482+ahyibrahim@users.noreply.github.com>
2020-11-11 19:03:09 +0200

    Complete Project

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
 const express = require('express');
@@ -21,3 +21,37 @@ app.listen(port, ()=>{console.log(`Server is up and listening on port ${port}`)}
 
 
 // Setup Server
+
+//Post route
+app.post('/addEntry', addEntry);
+
+function addEntry(req,res){
+    newEntry = {
+        date: req.body.date,
+        zip: req.body.zip,
+        temp: req.body.weatherData.main.temp,
+        climate: req.body.weatherData.weather[0].description,
+        content: req.body.content
+    }
+
+    projectData.push(newEntry);
+    res.send(projectData);
+    console.log('Posted Entry: \n' + newEntry);
+}
+
+//Get all route
+app.get('/all', getData);
+
+function getData(req,res){
+    res.send(projectData);
+    console.log('Sending All Data: \n' + JSON.stringify(projectData));
+}
+
+//Get Latest Entry Route
+app.get('/latest', getLatest);
+
+function getLatest(req,res){
+    const latestEntry = projectData[projectData.length - 1];
+    res.send(latestEntry);
+    console.log('Sending Latest Entry: ' + JSON.stringify(latestEntry));
+}
--- website/app.js
+++ website/app.js
@@ -6,25 +6,30 @@ let newDate = (d.getMonth()+1) + '/'+ d.getDate() +'/'+ d.getFullYear();
 const urlHeader = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const urlFooter = ',us&units=metric&appid=';
 const apiKey = '37b36ce4000c7679e923282b82a94786';
-//api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
-//api.openweathermap.org/data/2.5/weather?zip=85001,us&appid=685bd71bfc09cbca1c9bd72bbfe8c1cc
 
 function ini(){
     const button = document.getElementById('generate')
     button.addEventListener('click', () => {
         const zipCode = document.getElementById('zip');
         const zipCodeValue = zipCode.value;
-        const feelings = document.getElementById('feelings').value
+        const feelings = document.getElementById('feelings')
         let feelingsValue = feelings.value;
-
+        console.log('Feeling ' + feelingsValue)
         if (!isNaN(zipCodeValue) && !isNaN(parseInt(zipCodeValue)) && zipCodeValue.length == 5){
             zipCode.classList.remove('textError')
-            if (typeof feelingsValue == 'undefined')
+            if (typeof feelingsValue == 'undefined' || feelingsValue == "")
                 feelingsValue = 'Not in a sharing mood'
             const url = urlHeader+zipCodeValue+urlFooter+apiKey;
             console.log(feelingsValue)
-            getWeather(url, feelingsValue)
-            
+            getWeather(url).then((weatherData) => {
+                console.log(weatherData);
+                postData('/addEntry', {
+                    date: newDate,
+                    zip: zipCodeValue,
+                    weatherData: weatherData,
+                    content: feelingsValue
+                }).then(()=>{getData('/latest')});
+            });  
         }
 
         else{
@@ -45,14 +50,12 @@ function ini(){
 
 }
 
-const getWeather = async(url, content) => {
+const getWeather = async(url, feelings, zip) => {
     const res = await fetch(url);
     try{
         const data = await res.json();
-        console.log(data);
-        temp = data.main.temp + '°C - ' + data.weather[0].description;
-        populate(temp, content);
-
+        //const temp = data.main.temp + '°C - ' + data.weather[0].description;
+        return data
     } catch(error){
         console.log('Error: ' , error);
     }
@@ -63,5 +66,44 @@ function populate(temp, content){
     document.getElementById('temp').textContent = temp;
     document.getElementById('content').textContent = content; 
 }
+
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), 
+  });
+
+    try {
+      const newData = await response.json();
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    }
+}
+
+//This function will access all get routes on teh server. 
+//It defaults to the get all route, otherwise it will get the latest entry and populate the page with it
+const getData = async (url = '/all') => {
+    const request = await fetch(url);
+    try{
+        const data = await request.json();
+        console.log(data)
+        if (url == '/latest'){
+            console.log(data);
+            document.getElementById('date').textContent = "Date: " + data.date;
+            document.getElementById('temp').textContent = "Weather: " + data.temp + "°C - " + data.climate;
+            document.getElementById('content').textContent = "Feelings: " + data.content; 
+        } 
+    }catch(error){
+        console.log("Error: ", error);
+    }
+}
+
+
 
 ini();
